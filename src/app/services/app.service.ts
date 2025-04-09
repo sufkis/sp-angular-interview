1596ef6120e996a832666d128683689db8233a5e
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2025-04-09 04:24:08 +0300

    Add error handling

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 interface IAddress {
   username: string,
@@ -30,12 +30,17 @@ export class AppService {
   }
 
   getCities(): Observable<City[]> {
-    const response = this.http.get<City[]>(API_URL + '/cities');
+    const response = this.http.get<City[]>(API_URL + '/cities').pipe(catchError(this.handleError));
     return response;
   }
 
   sendAddress(address: IAddress): Observable<IAddress> {
-    const response = this.http.post<IAddress>(API_URL + '/submit', address);
+    const response = this.http.post<IAddress>(API_URL + '/submit', address).pipe(catchError(this.handleError));
     return response;
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred:', error);
+    return throwError(() => new Error('Something went wrong with the request. Please try again later.'));
   }
 }
